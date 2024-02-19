a786950a832fe121677016ee8a7e75af1f392532
Louis <l.cournollet@gmail.Com>
2024-02-19 14:49:56 +0100

    api implemented

---
+++ lib/api.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:unsplash_client/unsplash_client.dart';
+
+Future<Photo> fetchSinglePhoto() async {
+  // Load app credentials from environment variables or file.
+  var appCredentials = loadAppCredentialsFromEnv();
+
+  if (appCredentials == null) {
+    throw 'Please provide app credentials.';
+  }
+
+  // Create a client.
+  final client = UnsplashClient(
+    settings: ClientSettings(credentials: appCredentials),
+  );
+
+  // Fetch a single random photo.
+  final photo = await client.photos.random(count: 1).goAndGet();
+
+  // Close the client when it is done being used to clean up allocated resources.
+  client.close();
+
+  return photo.first;
+}
+
+/// Loads [AppCredentials] from environment variables
+/// (`UNSPLASH_ACCESS_KEY`, `UNSPLASH_SECRET_KEY`).
+///
+/// Returns `null` if the variables do not exist.
+AppCredentials? loadAppCredentialsFromEnv() {
+  // Hardcode the access key and secret key here
+  final accessKey = 'LapDdyGsYqIgC--edS3SQS0wf0kdDDoBAZIMEcjN09Y';
+  final secretKey = '2LhFVj9jQrdzWmdMdKouqg_FinL4TUEO8nFOwhWaGxg';
+
+  if (accessKey.isNotEmpty && secretKey.isNotEmpty) {
+    return AppCredentials(
+      accessKey: accessKey,
+      secretKey: secretKey,
+    );
+  }
+
+  return null;
+}
--- lib/imageOne.dart
+++ lib/imageOne.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pick_color/pick_color.dart';
+import 'package:color_picker/api.dart';
+import 'package:unsplash_client/unsplash_client.dart';
 
 class ImagePickerScreen extends StatefulWidget {
   @override
@@ -7,71 +9,90 @@ class ImagePickerScreen extends StatefulWidget {
 }
 
 class _ImagePickerScreenState extends State<ImagePickerScreen> {
-  Image image = Image.asset(
-    "assets/1693224745713.jpg",
-    height: 300,
-  );
+  late Future<Photo> _photoFuture;
   Color? color;
   PickerResponse? userResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    _photoFuture = fetchSinglePhoto();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Column(
-            children: [
-              ColorPicker(
-                  child: image,
-                  showMarker: true,
-                  onChanged: (response) {
-                    setState(() {
-                      userResponse = response;
-                      this.color = response.selectionColor;
-                    });
-                  }),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      "Selected Color  :-",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
+        body: FutureBuilder<Photo>(
+          future: _photoFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData) {
+              return Center(child: Text('No photo data available'));
+            } else {
+              final photo = snapshot.data!;
+              final resizedUrl = "${photo.urls.raw}&w=400&h=400"; // Resize the image to 400x400
+              return Column(
+                children: [
+                  ColorPicker(
+                    child: Image.network(resizedUrl), // Use the resized image
+                    showMarker: true,
+                    onChanged: (response) {
+                      setState(() {
+                        userResponse = response;
+                        this.color = response.selectionColor;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Selected Color:",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: userResponse?.selectionColor ?? Colors.red,
+                            border: Border.all(color: Colors.black, width: 1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      width: 10,
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Hex Code: ${userResponse?.hexCode ?? ""}",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                          color: userResponse?.selectionColor ?? Colors.red,
-                          border: Border.all(color: Colors.black, width: 1),
-                          borderRadius: BorderRadius.circular(20)),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text("Hex Code  :- ${userResponse?.hexCode ?? ""}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-              // ${userResponse?.hexCode ?? ""}
-            ],
-          )),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
