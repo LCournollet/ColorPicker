6406a71fc06d13e788f470d007318c89a5b7cadd
NSEREIR <nacim.sereir@ynov.com>
2024-02-19 12:17:28 +0100

    structure de base

---
+++ lib/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Color Picker'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to ColorPicker App!',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BottomNavItem(
+              title: 'mycolors',
+              onPressed: () {
+                print('mycolors button pressed');
+              },
+            ),
+            BottomNavItem(
+              title: 'Choose colors',
+              onPressed: () {
+                print('Choose colors button pressed');
+              },
+            ),
+            BottomNavItem(
+              title: 'IMAGE',
+              onPressed: () {
+                print('Image button pressed');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+
+  const BottomNavItem({
+    required this.title,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => onPressed(),
+      child: Text(
+        title,
+        style: const TextStyle(fontSize: 16.0, color: Colors.blue),
+      ),
+    );
+  }
+}
