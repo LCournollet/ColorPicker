763d12f787652ef642e88d02700fd42653e5c5eb
Louis <98102389+LCournollet@users.noreply.github.com>
2024-02-21 22:25:36 +0100

    Update imported_picture_screen.dart

--- lib/images/imported_picture_screen.dart
+++ lib/images/imported_picture_screen.dart
@@ -1,9 +1,11 @@
-import 'dart:io';
+// Page pour importer ou prendre une photo
 
-import 'package:flutter/material.dart';
-import 'package:pick_color/pick_color.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
+import 'dart:io'; // Importation de la bibliothèque permettant de travailler avec les fichiers
+
+import 'package:flutter/material.dart'; // Importation du package Flutter Material
+import 'package:pick_color/pick_color.dart'; // Importation du package pour le sélecteur de couleur
+import 'package:flutter/services.dart'; // Importation du package flutter/services.dart pour accéder à la fonctionnalité du presse-papiers
+import 'package:image_picker/image_picker.dart'; // Importation du package pour choisir des images depuis le périphérique
 
 class ImportedPictureScreen extends StatefulWidget {
   @override
@@ -11,27 +13,29 @@ class ImportedPictureScreen extends StatefulWidget {
 }
 
 class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
-  Color? color;
-  PickerResponse? userResponse;
-  bool _showRectangle = false;
-  final ImagePicker _imagePicker = ImagePicker();
-  File? _selectedImage;
+  Color? color; // Couleur sélectionnée
+  PickerResponse? userResponse; // Réponse de l'utilisateur lors de la sélection de couleur
+  bool _showRectangle = false; // Variable pour afficher ou masquer le rectangle de couleur
+  final ImagePicker _imagePicker = ImagePicker(); // Instance de la classe ImagePicker pour choisir des images
+  File? _selectedImage; // Fichier de l'image sélectionnée
 
+  // Fonction pour copier le code hexadécimal de la couleur dans le presse-papiers
   void _copyToClipboard(String text) {
-    Clipboard.setData(ClipboardData(text: text));
-    ScaffoldMessenger.of(context).showSnackBar(
+    Clipboard.setData(ClipboardData(text: text)); // Copie du texte dans le presse-papiers
+    ScaffoldMessenger.of(context).showSnackBar( // Affichage d'un SnackBar pour indiquer que le code a été copié
       SnackBar(
-        content: Text('Hex code copied to clipboard'),
+        content: Text('Code hexadécimal copié dans le presse-papiers'),
       ),
     );
   }
 
+  // Fonction pour choisir une image depuis la galerie ou l'appareil photo
   Future<void> _pickImage(ImageSource source) async {
-    final pickedImage = await _imagePicker.pickImage(source: source);
+    final pickedImage = await _imagePicker.pickImage(source: source); // Sélection de l'image
     if (pickedImage != null) {
       setState(() {
-        _selectedImage = File(pickedImage.path);
-        _showRectangle = false;
+        _selectedImage = File(pickedImage.path); // Assignation de l'image sélectionnée
+        _showRectangle = false; // Réinitialisation de l'affichage du rectangle
       });
     }
   }
@@ -41,20 +45,20 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Import or take a picture'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
+          title: Text('Importer ou prendre une photo'), // Titre de l'application dans la barre d'applications
+          centerTitle: true, // Centrage du titre
+          backgroundColor: Colors.blue, // Couleur de fond de la barre d'applications
         ),
-        backgroundColor: Color(0xFFCFF0FF),
+        backgroundColor: Color(0xFFCFF0FF), // Couleur de fond de l'écran
         body: Column(
           children: [
             Expanded(
               child: Stack(
                 alignment: Alignment.center,
                 children: [
-                  if (_selectedImage != null)
+                  if (_selectedImage != null) // Affichage de l'image sélectionnée si elle existe
                     Positioned(
-                      top: MediaQuery.of(context).size.height * 0.1,
+                      top: MediaQuery.of(context).size.height * 0.1, // Positionnement de l'image au milieu de l'écran
                       child: Image.file(
                         _selectedImage!,
                         width: 800,
@@ -65,7 +69,7 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
                   GestureDetector(
                     onTap: () {
                       setState(() {
-                        _showRectangle = true;
+                        _showRectangle = true; // Affichage du rectangle de sélection de couleur au toucher de l'écran
                       });
                     },
                     child: ColorPicker(
@@ -75,36 +79,36 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
                         decoration: BoxDecoration(
                           image: _selectedImage != null
                               ? DecorationImage(
-                            image: FileImage(_selectedImage!),
-                            fit: BoxFit.cover,
-                          )
+                                  image: FileImage(_selectedImage!),
+                                  fit: BoxFit.cover,
+                                )
                               : null,
                         ),
                       ),
-                      showMarker: false,
+                      showMarker: false, // Désactivation du marqueur de sélection de couleur
                       onChanged: (response) {
                         setState(() {
-                          userResponse = response;
-                          this.color = response.selectionColor;
-                          _showRectangle = true;
+                          userResponse = response; // Attribution de la réponse de l'utilisateur
+                          this.color = response.selectionColor; // Attribution de la couleur sélectionnée
+                          _showRectangle = true; // Affichage du rectangle de sélection de couleur
                         });
                       },
                     ),
                   ),
-                  if (_showRectangle && userResponse != null)
+                  if (_showRectangle && userResponse != null) // Affichage du rectangle de couleur sélectionnée
                     Positioned(
-                      left: userResponse!.xpostion+5,
-                      top: userResponse!.ypostion-25,
+                      left: userResponse!.xpostion + 5, // Positionnement horizontal du rectangle
+                      top: userResponse!.ypostion - 25, // Positionnement vertical du rectangle
                       child: Container(
                         width: 20,
                         height: 20,
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
-                          color: userResponse!.selectionColor,
+                          color: userResponse!.selectionColor, // Couleur du cercle dans le rectangle
                         ),
                       ),
                     ),
-                  if (_showRectangle)
+                  if (_showRectangle) // Affichage du rectangle avec la couleur sélectionnée et le code hexadécimal correspondant
                     Positioned(
                       bottom: 20,
                       child: Container(
@@ -112,12 +116,12 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
                         decoration: BoxDecoration(
                           border: Border.all(color: Colors.black, width: 1),
                           borderRadius: BorderRadius.circular(5),
-                          color: Colors.grey[200],
+                          color: Colors.grey[200], // Couleur de fond du rectangle
                         ),
                         child: InkWell(
                           onTap: () {
                             if (userResponse != null) {
-                              _copyToClipboard(userResponse!.hexCode);
+                              _copyToClipboard(userResponse!.hexCode); // Copie du code hexadécimal dans le presse-papiers
                             }
                           },
                           child: Column(
@@ -126,9 +130,9 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   Text(
-                                    "Selected Color:",
+                                    "Couleur sélectionnée:", // Libellé indiquant la couleur sélectionnée
                                     style: TextStyle(
-                                      color: Colors.blue, // Change la couleur du texte en bleu
+                                      color: Colors.blue, // Couleur du texte en bleu
                                       fontWeight: FontWeight.bold,
                                       fontSize: 16,
                                     ),
@@ -150,7 +154,7 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   Text(
-                                    "Hex Code: ${userResponse?.hexCode ?? "-"}",
+                                    "Code Hex: ${userResponse?.hexCode ?? "-"}", // Affichage du code hexadécimal de la couleur sélectionnée
                                     style: TextStyle(
                                       color: userResponse != null ? Colors.blue : Colors.grey,
                                       fontWeight: FontWeight.bold,
@@ -176,19 +180,19 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
                     children: [
                       ElevatedButton.icon(
                         onPressed: () => _pickImage(ImageSource.gallery),
-                        icon: Icon(Icons.image, color: Colors.blue), // Change la couleur de l'icône en bleu
+                        icon: Icon(Icons.image, color: Colors.blue), // Icône pour importer une image depuis la galerie
                         label: Text(
-                          'Import Picture',
-                          style: TextStyle(color: Colors.blue), // Change la couleur du texte en bleu
+                          'Importer une photo',
+                          style: TextStyle(color: Colors.blue), // Texte avec la couleur bleue
                         ),
                       ),
                       SizedBox(width: 16),
                       ElevatedButton.icon(
                         onPressed: () => _pickImage(ImageSource.camera),
-                        icon: Icon(Icons.camera_alt, color: Colors.blue), // Change la couleur de l'icône en bleu
+                        icon: Icon(Icons.camera_alt, color: Colors.blue), // Icône pour prendre une photo avec l'appareil photo
                         label: Text(
-                          'Take Picture',
-                          style: TextStyle(color: Colors.blue), // Change la couleur du texte en bleu
+                          'Prendre une photo',
+                          style: TextStyle(color: Colors.blue), // Texte avec la couleur bleue
                         ),
                       ),
                     ],
@@ -202,3 +206,4 @@ class _ImportedPictureScreenState extends State<ImportedPictureScreen> {
     );
   }
 }
+
