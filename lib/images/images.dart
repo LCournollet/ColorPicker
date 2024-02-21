d93b42ec7e59e437aa1c06c26f8fb96c73d6086a
Louis <98102389+LCournollet@users.noreply.github.com>
2024-02-21 22:12:31 +0100

    Update images.dart

--- lib/images/images.dart
+++ lib/images/images.dart
@@ -1,3 +1,5 @@
+//Page avec les deux boutons de sélection
+
 import 'package:flutter/material.dart';
 import 'package:color_picker/images/generate_image_screen.dart';
 import 'package:color_picker/images/imported_picture_screen.dart';
@@ -7,40 +9,40 @@ class ImagePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Images'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
+        title: Text('Images'), // Titre de la page
+        centerTitle: true, // Centrer le titre dans la barre d'applications
+        backgroundColor: Colors.blue, // Couleur de fond de la barre d'applications
       ),
-      backgroundColor: Color(0xFFCFF0FF),
+      backgroundColor: Color(0xFFCFF0FF), // Couleur de fond de la page
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center, // Centrer le bouton
           children: [
             ElevatedButton.icon(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => GenerateImageScreen()),
+                  MaterialPageRoute(builder: (context) => GenerateImageScreen()), // Class pour générer une image via l'api
                 );
               },
-              icon: Icon(Icons.image, color: Colors.red),
+              icon: Icon(Icons.image, color: Colors.red), // Icône de l'image en rouge
               label: Text(
-                'Generate an image',
-                style: TextStyle(color: Colors.red),
+                'Générer une image', // Texte du bouton pour générer une image
+                style: TextStyle(color: Colors.red), // Couleur du texte en rouge
               ),
             ),
-            SizedBox(height: 80),
+            SizedBox(height: 80), // Espacement vertical
             ElevatedButton.icon(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => ImportedPictureScreen()),
+                  MaterialPageRoute(builder: (context) => ImportedPictureScreen()), // Class pour importer ou prendre une photo depuis l'apareil
                 );
               },
-              icon: Icon(Icons.photo, color: Colors.green), // Change la couleur de l'icône en vert
+              icon: Icon(Icons.photo, color: Colors.green), // Icône de la photo en vert
               label: Text(
-                'Use my images',
-                style: TextStyle(color: Colors.green), // Change la couleur du texte en vert
+                'Utiliser mes images', // Texte du bouton pour utiliser mes images
+                style: TextStyle(color: Colors.green), // Couleur du texte en vert
               ),
             ),
           ],
@@ -49,3 +51,4 @@ class ImagePage extends StatelessWidget {
     );
   }
 }
+
